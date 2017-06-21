14372e62b576882e93a54224330271045a7d6688
neptune46 <neptune46@yeah.net>
2017-06-21 21:32:54 +0800

    remove test code

--- tm1637.py
+++ tm1637.py
@@ -1,9 +1,6 @@
 import RPi.GPIO as GPIO
 import time
 
-CLK = 11
-DIO = 12
-
 """
       A
      ---
@@ -136,27 +133,8 @@ class TM1637:
         self.i2c_start()
         self.i2c_write_byte(0x88) # set maxium brightness
         self.i2c_ack()
-        time.sleep(2)
+        time.sleep(1)
         self.i2c_stop()
 
-def main():
-    led = TM1637(CLK, DIO)
-    led.display_digit(['0', '1', '2', '3'])
-    led.display_digit(['4', '5', '6', '7'])
-    led.display_digit(['8', '9', 'A', 'B'])
-    led.display_digit(['C', 'D', 'E', 'F'])
-    led.display_digit(['-', '-', '-', '-'])
-    print "press any key to exit...wait for 2 seconds"
-    #r = raw_input()
-    time.sleep(1)
-    
-    
-if __name__ == "__main__": 
-    try:
-        main()
-    except KeyboardInterrupt: 
-        print "key board interrupt!"
-        #GPIO.cleanup()
-        
-    print "exit..."
-    GPIO.cleanup()
+
+
