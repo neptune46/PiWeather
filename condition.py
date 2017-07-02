4431eed0537f5be97ae98c2114684f691e35e920
neptune46 <neptune46@outlook.com>
2017-07-02 21:16:33 +0800

    add weather condition class

--- condition.py
+++ condition.py
@@ -2,5 +2,37 @@ import led
 import time
 import RPi.GPIO as GPIO
 
-class WeatherCondition():
-    __init__(self):
+disp_value = [
+    0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01, 0x00
+]
+
+class Condition:
+    def __init__(self, n):
+        self.__index = 8
+        self.__valid = True
+        if n == 0:
+            self.__led = led.led1
+        elif n == 1:
+            self.__led = led.led2
+        elif n == 2:
+            self.__led = led.led3
+        else:
+            print "invalid led number! valid range is 0~2"
+            self.__valid = False
+    
+    def __del__(self):
+        print "Deconstruct Condition ..."
+        
+    def display(self, index):
+        if self.__valid == True:
+            if index < 0 or index > 7:
+                self.__index = 8
+            else:
+                self.__index = index
+            value = disp_value[self.__index]
+            self.__led.setByte(value)
+
+    def clear(self):
+        if self.__valid == True:
+            self.__led.clear()
+
--- weather.py
+++ weather.py
@@ -1,7 +1,17 @@
 import led
 import temperature
+import condition
 import time
 import RPi.GPIO as GPIO
+
+CODE_SUNNY = 0
+CODE_CLOUDY = 1
+CODE_PARTLY_CLOUDY = 2
+CODE_OVERCAST = 3
+CODE_SHOWER_RAIN = 4
+CODE_LIGHT_RAIN = 5
+CODE_MODERATE_RAIN = 6
+CODE_HEAVY_RAIN = 7
 
 def testLED():
     GPIO.setmode(GPIO.BOARD)
@@ -17,6 +27,37 @@ def testLED():
     led2.clear()
     led3.clear()
 
+def testCondition():
+    condi1 = condition.Condition(0)
+    condi2 = condition.Condition(1)
+    condi3 = condition.Condition(2)
+
+    condi1.display(CODE_SUNNY)
+    time.sleep(1)
+    condi2.display(CODE_PARTLY_CLOUDY)
+    time.sleep(1)
+    condi3.display(CODE_HEAVY_RAIN)
+    time.sleep(1)
+
+    condi1.display(CODE_CLOUDY)
+    time.sleep(1)
+    condi2.display(CODE_OVERCAST)
+    time.sleep(1)
+    condi3.display(CODE_MODERATE_RAIN)
+    time.sleep(1)
+
+    condi1.display(CODE_SHOWER_RAIN)
+    time.sleep(1)
+    condi2.display(CODE_SHOWER_RAIN)
+    time.sleep(1)
+    condi3.display(CODE_SUNNY)
+    time.sleep(1)
+    
+    condi1.clear()
+    condi2.clear()
+    condi3.clear()
+
+
 def testAirTemp():
     airTemp1 = temperature.tempDisp1
     airTemp2 = temperature.tempDisp2
@@ -26,8 +67,9 @@ def testAirTemp():
     temperature.test(airTemp3)
 
 def main():
-    testLED()
-    testAirTemp()
+    #testLED()
+    #testAirTemp()
+    testCondition()
     
 if __name__ == "__main__": 
     try:
